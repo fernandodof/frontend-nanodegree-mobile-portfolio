e93e450b0842e3bf0e8bfac88663daa64e9a6b78
Fernando <fernandodof@gmail.com>
2017-02-28 15:15:50 -0300

    refactor: Optizing website experience

--- README.md
+++ README.md
@@ -1,40 +1,37 @@
 ## Website Performance Optimization portfolio project
 
-Your challenge, if you wish to accept it (and we sure hope you will), is to optimize this online portfolio for speed! In particular, optimize the critical rendering path and make this page render as quickly as possible by applying the techniques you've picked up in the [Critical Rendering Path course](https://www.udacity.com/course/ud884).
-
-To get started, check out the repository and inspect the code.
+This project is a clone of Udacity's [frontend-nanodegree-mobile-portfolio](https://github.com/udacity/frontend-nanodegree-mobile-portfolio) and optimized based on the [Critical Rendering Path course](https://www.udacity.com/course/ud884).
 
 ### Getting started
 
-####Part 1: Optimize PageSpeed Insights score for index.html
+After having cloned this project, got to its root folder:
 
-Some useful tips to help you get started:
+To Run a development version
+* `$ gulp run` to run the project
+* go to http://localhost:7083/
 
-1. Check out the repository
-1. To inspect the site on your phone, you can run a local server
+To Run a dist version
+* `$ gulp build` to generate dist files
+* `$ gulp run-dist` to run the project
+* go to http://localhost:8888/
 
-  ```bash
-  $> cd /path/to/your-project-folder
-  $> python -m SimpleHTTPServer 8080
-  ```
+### Optimizations made
 
-1. Open a browser and visit localhost:8080
-1. Download and install [ngrok](https://ngrok.com/) to the top-level of your project directory to make your local server accessible remotely.
+##### global
 
-  ``` bash
-  $> cd /path/to/your-project-folder
-  $> ./ngrok http 8080
-  ```
+* Moved scripts to the end of the pages
+* Minified JS files and concatenated into a single file
+* Minified CSS files and concatenated into a single file
+* Minified HTML files
+* Optimized images
+* Created servers with gulp
+* Integrated ngrok
 
-1. Copy the public URL ngrok gives you and try running it through PageSpeed Insights! Optional: [More on integrating ngrok, Grunt and PageSpeed.](http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/)
+###### pizzas
 
-Profile, optimize, measure... and then lather, rinse, and repeat. Good luck!
-
-####Part 2: Optimize Frames per Second in pizza.html
-
-To optimize views/pizza.html, you will need to modify views/js/main.js until your frames per second rate is 60 fps or higher. You will find instructive comments in main.js. 
-
-You might find the FPS Counter/HUD Display useful in Chrome developer tools described here: [Chrome Dev Tools tips-and-tricks](https://developer.chrome.com/devtools/docs/tips-and-tricks).
+* Optimized pizzas creation
+* Optimized scrolling
+* Optimized size changes
 
 ### Optimization Tips and Tricks
 * [Optimizing Performance](https://developers.google.com/web/fundamentals/performance/ "web performance")
@@ -47,9 +44,3 @@ You might find the FPS Counter/HUD Display useful in Chrome developer tools desc
 * <a href="https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/optimize-encoding-and-transfer.html">Reduce the size of text</a>
 * <a href="https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/image-optimization.html">Optimize images</a>
 * <a href="https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching.html">HTTP caching</a>
-
-### Customization with Bootstrap
-The portfolio was built on Twitter's <a href="http://getbootstrap.com/">Bootstrap</a> framework. All custom styles are in `dist/css/portfolio.css` in the portfolio repo.
-
-* <a href="http://getbootstrap.com/css/">Bootstrap's CSS Classes</a>
-* <a href="http://getbootstrap.com/components/">Bootstrap's Components</a>
---
+++ gulpfile.js
@@ -0,0 +1,184 @@
+//Dependencies used for building
+var gulp = require('gulp');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
+var imagemin = require('gulp-imagemin');
+
+//Paths used by gulp tasks
+var paths = {
+    index: 'index.html',
+    project2048: 'project-2048.html',
+    projectMobile: 'project-mobile.html',
+    projectWebperf: 'project-webperf.html',
+    dist: 'dist/',
+    global: {
+        html: {
+            src: '*.html',
+            dist: 'dist/'
+        },
+        css: {
+            src: 'css/*',
+            dist: 'dist/css'
+        },
+        js: {
+            src: 'js',
+            dist: 'dist/js'
+        },
+        img: {
+            src: 'img/**/*.*',
+            dist: 'dist/img/'
+        }
+    },
+    pizza: 'views/pizza.html',
+    distViews: 'dist/views',
+    views: {
+        html: {
+            src: 'views/*.html',
+            dist: 'dist/views/'
+        },
+        css: {
+            src: 'views/css/*',
+            dist: 'dist/views/css/'
+        },
+        js: {
+            src: 'js',
+            dist: 'dist/views/js/'
+        },
+        img: {
+            src: 'views/images/**/*.*',
+            dist: 'dist/views/images/'
+        }
+    }
+};
+
+//Ports used for running the application
+var ports = {
+    dev: '7083',
+    dist: '8888'
+};
+
+//Task used to delete dist folder before building again
+gulp.task('clean', function() {
+    return del.sync([
+        'dist/'
+    ]);
+});
+
+//Minifying global images
+gulp.task('imagemin-global', function() {
+    gulp.src(paths.global.img.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.global.img.dist));
+});
+
+//Minifying html, css, and js on index.html
+gulp.task('usemin-index', function() {
+    return gulp.src(paths.index)
+        .pipe(usemin({
+            css: [cleanCSS({
+                compatibility: 'ie8'
+            }), 'concat'],
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), 'concat'],
+            inlinejs: [uglify()],
+        }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+//Minifying html, css, and js on project-2048.html
+gulp.task('usemin-project2048', function() {
+    return gulp.src(paths.project2048)
+        .pipe(usemin({
+            css: [cleanCSS({
+                compatibility: 'ie8'
+            }), 'concat'],
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), 'concat'],
+            inlinejs: [uglify()],
+        }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+//Minifying html, css, and js on project-mobile.html
+gulp.task('usemin-projectMobile', function() {
+    return gulp.src(paths.projectMobile)
+        .pipe(usemin({
+            css: [cleanCSS({
+                compatibility: 'ie8'
+            }), 'concat'],
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), 'concat'],
+            inlinejs: [uglify()],
+        }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+//Minifying html, css, and js on project-webperf.html
+gulp.task('usemin-projectWebperf', function() {
+    return gulp.src(paths.projectWebperf)
+        .pipe(usemin({
+            css: [cleanCSS({
+                compatibility: 'ie8'
+            }), 'concat'],
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), 'concat'],
+            inlinejs: [uglify()],
+        }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+//Minifying images on view page
+gulp.task('imagemin-views', function() {
+    gulp.src(paths.views.img.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.views.img.dist));
+});
+
+//Minifying html, css, and js on pizza.html
+gulp.task('usemin-pizza', function() {
+    return gulp.src(paths.pizza)
+        .pipe(usemin({
+            css: [cleanCSS({
+                compatibility: 'ie8'
+            }), 'concat'],
+            html: [htmlmin({
+                collapseWhitespace: true
+            })],
+            js: [uglify(), 'concat'],
+            inlinejs: [uglify()],
+        }))
+        .pipe(gulp.dest(paths.distViews));
+});
+
+//Creating server for development testing
+gulp.task('run', function() {
+    connect.server({
+        root: __dirname + '/',
+        livereload: true,
+        port: ports.dev
+    });
+});
+
+//Creating server for dist testing
+gulp.task('run-dist', function() {
+    connect.server({
+        root: 'dist',
+        livereload: true,
+        port: ports.dist
+    });
+});
+
+//Calling Build Tasks
+gulp.task('build', ['clean', 'imagemin-global', 'usemin-index', 'usemin-project2048', 'usemin-projectMobile', 'usemin-projectWebperf', 'imagemin-views', 'usemin-pizza']);
